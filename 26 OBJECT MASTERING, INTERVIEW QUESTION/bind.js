f6e3e4c3d6e3c69630ee98388770d4b09763df98
Partho <parthoo.rahman@gmail.com>
2022-04-21 13:03:28 +0600

    object bind + call + apply

--- 26 OBJECT MASTERING, INTERVIEW QUESTION/bind.js
+++ 26 OBJECT MASTERING, INTERVIEW QUESTION/bind.js
@@ -26,15 +26,52 @@ const thirdPerson = {
   salary: 100000,
 };
 
+// bind
+
 const secondPersonBill = firstPerson.chargeBill.bind(secondPerson);
 secondPersonBill(2000);
 secondPersonBill(7000);
-console.log("Second person salary after charge bill: " + secondPerson.salary);
+console.log("(bind) Second person salary after charge bill : " + secondPerson.salary);
 
 const thirdPersonbill = firstPerson.chargeBill.bind(thirdPerson);
 thirdPersonbill(3000);
 thirdPersonbill(500);
-console.log("Third Person salary after charge bill : " + thirdPerson.salary);
+console.log("(bind) Third Person salary after charge bill : " + thirdPerson.salary);
+
+/////////////////////////////////////
+
+// Call
+
+firstPerson.chargeBill.call(secondPerson, 900);
+console.log("(call) Second person salary after charge bill: " + secondPerson.salary);
 
 
+const forthPerson = {
+  firstName: "ADV",
+  lastName: "MUSA",
+  salary: 200000,
+  getFullName: function () {
+    console.log(this.firstName, this.lastName);
+  },
+  chargeBill: function (amount, tax, tips) {
+    // console.log(this);
+    this.salary = this.salary - amount - tax - tips;
+    return this.salary;
+  }
+};
 
+const fifthPerson = {
+  firstName: "Dablu",
+  lastName: "Rahman",
+  salary: 10000,
+};
+
+forthPerson.chargeBill.call(fifthPerson, 900, 100, 20);
+console.log("(call with multy perameter) Second person salary after charge bill: " + fifthPerson.salary);
+
+/////////////////////////////////////
+
+// apply
+
+forthPerson.chargeBill.apply(forthPerson, [900, 100, 20]);
+console.log("(apply with multy perameter) Second person salary after charge bill: " + fifthPerson.salary);
